3b91596669ddc60095127053d2ed97cfe69d075a
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-12-05 15:40:01 +0800

    fix: comments

--- src/content-scripts/statutes/get-citation/get-provision/get-provision.ts
+++ src/content-scripts/statutes/get-citation/get-provision/get-provision.ts
@@ -4,7 +4,7 @@ import { ProvisionGraph } from "./stringify-provision-map";
 /**
  * Two steps to get the provision
  * 1. Get the regex matches while traversing upwards through the DOM, for each left click target HTMLElement and right click target HTMLElement
- * 2. Create a graph of nodes, with each node representing a provision, e.g. 6 -> (1) -> [(a), (b)]
+ * 2. Create a graph of nodes, with each node representing a provision, e.g. 6 -> (1) -> [(a), (b)], refers to s 6(1)(a) to s 6(1)(b)
  * @param leftClick The HTML target element from the left click event
  * @param rightClick The HTML target element from the right click event
  * @returns The fully formed provision
