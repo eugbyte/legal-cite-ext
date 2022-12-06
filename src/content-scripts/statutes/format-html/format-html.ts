ac4394040647b1cad045bfcfaf3368b65dbd460c
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-12-06 17:54:02 +0800

    chore: update

--- src/content-scripts/statutes/format-html/format-html.ts
+++ src/content-scripts/statutes/format-html/format-html.ts
@@ -1,17 +1,35 @@
-import { bracketAlpha } from "../get-citation";
-
 export const formatHTML = (selection: string, citation: string) => {
-  if (bracketAlpha.test(citation)) {
-    const texts = citation.split(bracketAlpha);
-    const [bracketAlphaText] = bracketAlpha.exec(citation) as RegExpExecArray;
-    bracketAlphaText.replace("(", "<i>");
-    bracketAlphaText.replace(")", "</i>");
-    texts.splice(1, 0, bracketAlphaText);
-    citation = texts.join("");
+  const bracketAlpha = /\([A-Z]+\)/gi;
+
+  const queue: [number, number][] = [...citation.matchAll(bracketAlpha)].map(
+    (match) => [
+      match.index as number,
+      (match.index as number) + match[0].length - 1,
+    ]
+  );
+
+  let result = "";
+  let index = 0;
+  while (index < citation.length && queue.length > 0) {
+    const [start, end] = queue[0];
+    const inRange = start <= index && index <= end;
+    if (!inRange) {
+      result += citation[index];
+      index += 1;
+    } else {
+      queue.shift();
+      result += `<i>${citation.slice(start, end + 1)}</i>`;
+      index += end - start + 1;
+    }
   }
+
+  if (index != citation.length) {
+    result += citation.slice(index);
+  }
+
   const htmlContent = `
         <p>${selection}</p>
-        <p style="color:red">${citation}</p>
+        <p style="color:red">${result}</p>
       `;
 
   return htmlContent;
