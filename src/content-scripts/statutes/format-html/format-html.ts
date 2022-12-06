010dda9c36dd395c0f4a45a0368bec869fe732fa
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-12-06 18:28:56 +0800

    chore: update

--- src/content-scripts/statutes/format-html/format-html.ts
+++ src/content-scripts/statutes/format-html/format-html.ts
@@ -27,7 +27,8 @@ export const formatHTML = (selection: string, citation: string) => {
       queue.shift();
       let italic = citation.slice(start, end + 1); // "(e)"
       italic = italic.slice(1, italic.length - 1); // "e"
-      result += `(<i>${italic}</i>)`; // "(<i>e</i>)"
+      italic = `<i>${italic}</i>` // "<i>e</i>"
+      result += `(${italic})`; // "(<i>e</i>)"
       index += end - start + 1;
     }
   }
