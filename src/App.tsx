eefd0cb786930fe5a9ea9999a2e09b84d3fdfe7d
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-12-07 13:20:43 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div className="bg-slate-800 flex flex-col justify-center items-center p-5">
-      <p className="text-white text-sm decoration-solid underline ">How to use</p>
+    <div className="bg-slate-800 flex flex-col justify-center items-center px-5 py-1 text-white">
+      <p className="text-sm decoration-solid underline ">How to use</p>
+      <div>
+        <ol className="text-xs list-decimal">
+          <li>
+            Navigate to a whitelisted website, e.g.{" "}
+            <code>https://sso.agc.gov.sg/Act/**</code>
+          </li>
+          <li>Select the text</li>
+          <li>Right click, select "Copy with Source" from the context menu</li>
+        </ol>
+      </div>
     </div>
   );
 }
