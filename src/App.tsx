8798940bc6d222e0d375580409492cec1b1a7c6a
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-12-07 13:00:25 +0800

    feat: ui popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
-  console.log("in pop up script");
   return (
-    <div className="bg-slate-800 flex flex-col justify-center items-center">
-      <h1>About</h1>
-      <p>How to use</p>
+    <div className="bg-slate-800 flex flex-col justify-center items-center p-5">
+      <p className="text-white text-sm decoration-solid underline ">How to use</p>
     </div>
   );
 }
