9efef5c5f6d96d6fbfb9dc45cf3da9c6fef0ea73
eugene <eugenetham1994@gmail.com>
2023-09-30 13:51:47 +0800

    fix: width

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-slate-800 flex flex-col justify-center items-center px-5 py-1 text-white">
+    <div className="w-80 bg-slate-800 flex flex-col justify-center items-center px-5 py-1 text-white">
       <p className="text-sm decoration-solid underline ">How to use</p>
       <div>
         <ol className="text-xs list-decimal">
@@ -9,7 +9,7 @@ function App() {
             <code>https://sso.agc.gov.sg/Act/**</code>
           </li>
           <li>Select the text</li>
-          <li>Right click, select "Copy with Source" from the context menu</li>
+          <li>Right click, select "Copy with Citation" from the context menu</li>
         </ol>
       </div>
     </div>
